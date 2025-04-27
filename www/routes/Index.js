ae30ba63da29477be7b2977fa620467fd9c1392d
Sefinek <contact@sefinek.net>
2025-04-27 09:09:46 +0200

    Fix

--- www/routes/Index.js
+++ www/routes/Index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const MainController = require('../controllers');
+const MainController = require('../controllers/Index.js');
 
 router.get('/', MainController.index);
 router.get('/false-positives', MainController.falsePositives);
