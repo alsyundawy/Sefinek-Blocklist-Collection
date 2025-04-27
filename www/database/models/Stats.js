909ac4f54671fa76e0aeaf93d4225a0b2ef82ac4
Sefinek <contact@sefinek.net>
2025-04-27 03:50:51 +0200

    Update

--- www/database/models/Stats.js
+++ www/database/models/Stats.js
@@ -1,24 +1,26 @@
 const { Schema, model } = require('mongoose');
 
+const CategoriesSchema = new Schema({
+	'0000': { type: Number, default: 0 },
+	'127001': { type: Number, default: 0 },
+	adguard: { type: Number, default: 0 },
+	dnsmasq: { type: Number, default: 0 },
+	noip: { type: Number, default: 0 },
+	rpz: { type: Number, default: 0 },
+	unbound: { type: Number, default: 0 },
+}, { timestamps: false, _id: false });
+
 const StatsSchema = new Schema({
 	total: { type: Number, default: 0 },
 	blocklists: { type: Number, default: 0 },
 
-	perDay: { type: Map, of: Number, default: {} },
-	perMonth: { type: Map, of: Number, default: {} },
-	perYear: { type: Map, of: Number, default: {} },
+	perDay: { type: Map, of: Number, default: () => ({}) },
+	perMonth: { type: Map, of: Number, default: () => ({}) },
+	perYear: { type: Map, of: Number, default: () => ({}) },
 
-	categories: {
-		'0000': { type: Number, default: 0 },
-		'127001': { type: Number, default: 0 },
-		adguard: { type: Number, default: 0 },
-		dnsmasq: { type: Number, default: 0 },
-		noip: { type: Number, default: 0 },
-		rpz: { type: Number, default: 0 },
-		unbound: { type: Number, default: 0 },
-	},
+	categories: { type: CategoriesSchema, default: () => ({}) },
 
-	responses: { type: Map, of: Number, default: {} },
+	responses: { type: Map, of: Number, default: () => ({}) },
 	updateStatsFail: { type: Number, default: 0 },
 }, { timestamps: true, versionKey: false });
 
