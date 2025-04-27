802d72838a2c64452335c9855ef7ff612688ac2b
Sefinek <contact@sefinek.net>
2025-04-27 03:53:06 +0200

    Update

--- www/middlewares/ratelimit.js
+++ www/middlewares/ratelimit.js
@@ -3,7 +3,7 @@ const error = require('./other/errors.js');
 
 const limiter = rateLimit({
 	windowMs: 2 * 60 * 1000,
-	limit: 224,
+	limit: 225,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
 
