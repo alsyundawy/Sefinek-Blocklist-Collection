857c2d3ada46ee28f409161f5c3976706e93d728
Sefinek <contact@sefinek.net>
2025-04-28 07:01:07 +0200

    Fix

--- scripts/prepare-templates.js
+++ scripts/prepare-templates.js
@@ -36,7 +36,7 @@ const processDirectory = async dirPath => {
 
 			for (let line of lines) {
 				line = line.trim();
-				if (!line || line === '0.0.0.0') {
+				if (!line || line === '0.0.0.0' || line === '127.0.0.1') {
 					stats.invalidLinesRemoved++;
 					continue;
 				}
